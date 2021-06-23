27a93a29c729ac83a3f214c2787b086f278a734f
davoplus <davoplus@gmail.com>
2021-06-23 06:53:54 -0500

    Create postgresql.py

---
+++ postgresql.py
@@ -0,0 +1,35 @@
+import psycopg2
+import secrets
+import json
+
+def postgresqlConnection(dbJsonData):
+    
+    ENDPOINT=dbJsonData['host']
+    PORT=dbJsonData['port']
+    USR=dbJsonData['username']
+    DBNAME=dbJsonData['dbname']
+    PASSWD =dbJsonData['password']
+    
+    
+    try:
+        conn =  psycopg2.connect(user=USR, passwd=PASSWD,host=ENDPOINT, port=PORT, database=DBNAME)
+        cur = conn.cursor()
+        cur.execute("""SELECT * FROM Employee""")
+        myresult = cur.fetchall()
+
+        for x in myresult:
+          print(x)
+        
+    except Exception as e:
+        print("Database connection failed due to {0}".format(e))  
+    finally:
+      if conn.is_connected():
+        conn.close()
+        cur.close()
+        print("Postgress connection is closed")
+
+secret_name = sys.argv[1]
+region_name = "us-west-2"
+
+dbInfoString = secrets.get_secret(secret_name, region_name)
+print ( dbInfoString )
